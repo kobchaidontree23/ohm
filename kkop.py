c21bd922a8ee281269b41d75551fb6625fead1fb
kobchai <kobchaidontree23@gmail.com>
2021-08-05 17:12:24 +0700

    ups

--- kkop.py
+++ kkop.py
@@ -1,3 +1,20 @@
-a = "hello"
-print(a)
-b = "gg"
+#1
+subject_id = input()
+grade = input()
+print("i got",grade,"in",subject_id )
+
+#2
+subject_id,grade = input().split()
+print("i got",grade,"in",subject_id )
+
+#3
+n1 = (input())
+n2 = (input())
+n3 = (input())
+result = int(n1) + int(n2) +int(n3)
+print("result:",n1,"+",n2,"+",n3,"=",str(result))
+
+#4
+n1,n2,n3 = input().split()
+result = int(n1)+int(n2)+int(n3)
+print("result:",n1,"+",n2,"+",n3,"=",str(result))
